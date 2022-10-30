00b674f9d0fd1d79bf63c4521e3ec7ff5fa73f30
rohanku <rohankumar@berkeley.edu>
2022-10-29 18:10:22 -0700

    push fix

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+from skimage import restoration
 
 DATA_PATH = "../data"
 
@@ -10,10 +11,20 @@ def load_psf():
 def load_diffuser_image(n):
     return np.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), DATA_PATH, 'diffuser', f'im{n}.npy'))
 
-def load_diffuser_image(n):
+def load_lensed_image(n):
     return np.load(os.path.join(os.path.dirname(os.path.realpath(__file__)), DATA_PATH, 'lensed', f'im{n}.npy'))
 
 def display_array(arr):
     plt.figure()
     plt.imshow(arr)
     plt.show()
+
+def deconvolve(psf, measured_data):
+    return restoration.richardson_lucy(measured_data, psf)
+
+if __name__ == "__main__":
+    psf = load_psf()
+    measured_data = load_diffuser_image(10)
+
+    result = deconvolve(psf, measured_data)
+    display_array(result)
