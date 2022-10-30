7ed758cc9c3423fd4a6c90813b6e8981dce26b49
Rahul Kumar <rahulkumar@berkeley.edu>
2022-10-29 19:23:51 -0700

    add admm.py

--- src/wiener.py
+++
@@ -1,7 +0,0 @@
-from skimage import color, data, restoration
-from utils import load_psf, load_diffuser_image, display_array
-
-img = load_diffuser_image(10)
-psf = load_psf()
-deconvolved_img = restoration.wiener(img, psf, 0.1)
-display_array(deconvolved_img)
