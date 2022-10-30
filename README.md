486bae983b54d4c1f31bc2faee9b86f99331a222
Rahul Kumar <rahulkumar@berkeley.edu>
2022-10-29 18:12:16 -0700

    Add gitignore, add matplotlib to README, update Wiener deconv

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ To set up your development environment using conda:
 ```bash
 conda create -n ee290 python=3.10
 conda activate ee290
-python -m pip install numpy scikit-image black
+python -m pip install numpy scikit-image black matplotlib
 ```
 
 To format your code:
--- src/wiener.py
+++ src/wiener.py
@@ -1,6 +1,7 @@
 from skimage import color, data, restoration
+from utils import load_psf, load_diffuser_image, display_array
 
-img = load_image()
+img = load_diffuser_image(10)
 psf = load_psf()
 deconvolved_img = restoration.wiener(img, psf, 0.1)
-display_image(deconvolved_img)
+display_array(deconvolved_img)
