31dd84af1f1c88c1c182f7729e6088b7f26b1e07
rohanku <rohankumar@berkeley.edu>
2022-11-13 18:29:30 -0800

    set up for training on 1000 images

--- ml/train.py
+++ ml/train.py
@@ -29,9 +29,9 @@ var_options = {'plain_admm': [],
                'mu_and_tau': ['mus', 'tau'],
               }
 
-num_images = 2
+num_images = 1000
 trainset = DiffuserDataset_preprocessed_number(CSV_PATH, DATA_DIR, LABEL_DIR, None, num_images, transform=ToTensor())
-trainloader = torch.utils.data.DataLoader(trainset, batch_size = 1, shuffle=True, num_workers=0)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size = 1, shuffle=True, num_workers=2)
 
 learning_options_admm = {'learned_vars': var_options['mu_and_tau']} 
 
@@ -80,7 +80,7 @@ for epoch in range(2):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
+        if i % 100 == 99:    # print every 2000 mini-batches
             print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
             running_loss = 0.0
 
