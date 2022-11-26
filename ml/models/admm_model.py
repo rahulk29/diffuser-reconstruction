fix cropping logic and get gradient descent working for black and white images

ml/models/admm_model.py
@@ -5,11 +5,14 @@ from admm_rgb_pytorch import *
 import admm_filters_no_soft as admm_s
 
 class ADMM_Net(nn.Module):
-    def __init__(self, batch_size, h, iterations, learning_options = {'learned_vars': []}, 
+    def __init__(self, batch_size, h, iterations, fill_factor = 1, nx = 1, ny = 1, learning_options = {'learned_vars': []}, 
                  cuda_device = torch.device('cpu'), le_admm_s = False, denoise_model = []):
         super(ADMM_Net, self).__init__()
         
         self.iterations = iterations              # Number of unrolled iterations
+        self.fill_factor = fill_factor
+        self.nx = nx
+        self.ny = ny
         self.batch_size = batch_size              # Batch size 
         self.autotune = False                     # Using autotune (True or False)
         self.realdata = True                      # Real Data or Simulated Measurements
@@ -109,7 +112,20 @@ class ADMM_Net(nn.Module):
         
             
         Cty = pad_zeros_torch(self, y)                      # Zero padded input
-        CtC = pad_zeros_torch(self, torch.ones_like(y))     # Zero padded ones 
+        CtC = torch.zeros_like(y)     # Crop mask
+
+        w, h = CtC.shape
+        for i in range(self.nx):
+            start_i, end_i = w // self.nx * i, math.floor(
+                w // self.nx * (i + self.fill_factor)
+            )
+            for j in range(self.ny):
+                start_j, end_j = h // self.ny * j, math.floor(
+                    h // self.ny * (j + self.fill_factor)
+                )
+                CtC[start_i:end_i, start_j:end_j] = 1
+
+        CtC = pad_zeros_torch(self, CtC)
         
         # Create list of inputs/outputs         
         in_vars = []; in_vars1 = []

src/gd.py
@@ -60,11 +60,11 @@ class GDSolver:
     def crop_array(self, X):
         w, h = X.shape
         X_out = np.zeros(X.shape)
-        for i in range(w // self.nx):
+        for i in range(self.nx):
             start_i, end_i = w // self.nx * i, math.floor(
                 w // self.nx * (i + self.fill_factor)
             )
-            for j in range(h // self.ny):
+            for j in range(self.ny):
                 start_j, end_j = h // self.ny * j, math.floor(
                     h // self.ny * (j + self.fill_factor)
                 )
@@ -72,8 +72,16 @@ class GDSolver:
         return X_out
 
     def load_data(self):
-        self.psf = np.load(self.psf_file).astype("float32")[:, :, self.channel]
-        self.data = np.load(self.data_file).astype("float32")[:, :, self.channel]
+        self.psf = np.load(self.psf_file).astype("float32")
+        self.data = np.load(self.data_file).astype("float32")
+
+        if len(self.psf.shape) == 2:
+            self.psf = np.dstack([self.psf] * 3)
+        if len(self.data.shape) == 2:
+            self.data = np.dstack([self.data] * 3)
+
+        self.psf = self.psf[:, :, self.channel]
+        self.data = self.data[:, :, self.channel]
 
         # Subtract non-trivial background
         bg = np.mean(self.psf[5:15, 5:15])
@@ -183,12 +191,13 @@ class GDSolver:
 
 if __name__ == "__main__":
     images = []
-    for channel in range(3):
-        print(f"channel = {channel}")
-        gd_solver = GDSolver(fill_factor=0.8, channel=channel)
-        images.append(gd_solver.run())
+    # for channel in range(3):
+    #     print(f"channel = {channel}")
+    #     gd_solver = GDSolver(fill_factor=0.8, channel=channel)
+    #     images.append(gd_solver.run())
+    gd_solver = GDSolver(f=0.25, fill_factor=0.8, psf_file="../../DiffuserCam-Tutorial/tutorial/psf_sample.npy", data_file="../../DiffuserCam-Tutorial/tutorial/rawdata_hand_sample.npy")
 
-    image = np.stack(images, axis=-1)
+    image = gd_solver.run()
 
     utils.display_array(image, "Final reconstruction", cmap="gray")
 